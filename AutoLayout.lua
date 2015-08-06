b7667aceefcb2a8fc1dc3aba1496ea627115ef3b
justbilt <wangbilt@gmail.com>
2015-08-06 18:40:13 +0800

    first commit

---
+++ AutoLayout.lua
@@ -0,0 +1,138 @@
+--
+-- Author: justbilt
+-- Date: 2015-02-25 14:41:30
+--
+
+
+local BoxLayout = import(".BoxLayout")
+
+local AutoLayout = class("AutoLayout", cc.ui.UIScrollView)
+
+function AutoLayout:ctor()
+	AutoLayout.super.ctor(self)
+	self.box = BoxLayout.new()
+	self.box:setAnchorPoint(cc.p(0.0,0))
+	self:addScrollNode(self.box)
+end
+
+function AutoLayout:setSizeSuitEnable(_enbale)
+	self.sizeSuitEnbale = _enbale
+	self:refreshSizeSuit()
+end
+
+function AutoLayout:clear()
+	self.box:clear()
+end
+
+function AutoLayout:count()
+	return self.box:count()
+end
+
+function AutoLayout:insert(_item, _index, _params)
+	return self.box:insert(_item, _index, _params)
+end
+
+function AutoLayout:push(_item, _params)
+	return self.box:push(_item, _params)
+end
+
+function AutoLayout:pop()
+	self.box:pop()
+end
+
+function AutoLayout:removeByTag(_tag, _all)
+	self.box:removeByTag(_tag, _all)
+end
+
+function AutoLayout:remove(_id)
+	self.box:remove(_id)
+end
+
+function AutoLayout:getViewSize()
+	return self:getContentSize()
+end
+
+function AutoLayout:setViewSize(_size)
+	self:setViewRect(cc.rect(0,0,_size.width,_size.height))
+	self:setContentSize(_size)
+end
+
+function AutoLayout:gotoBegin(_ani)
+	if self:getDirection() == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+		self:setContentOffset(-self.box:getContentSize().height+self:getViewSize().height, _ani)
+	else
+		self:setContentOffset(0, _ani)
+	end
+end
+
+function AutoLayout:gotoEnd(_ani)
+	if self:getDirection() == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+		self:setContentOffset(math.max(0, self:getViewSize().height -self.box:getContentSize().height ), _ani)
+	else
+		self:setContentOffset(-self.box:getContentSize().width+self:getViewSize().width, _ani)
+	end
+end
+
+function AutoLayout:setContentOffset(offset, animated)
+	if self:getDirection() == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+		self.scrollNode:setPositionY(offset)
+	else
+		self.scrollNode:setPositionX(offset)
+	end	
+end
+
+function AutoLayout:pushGrid(_item_list, _stripe, _direction, _params, _padding)
+	return self.box:pushGrid(_item_list, _stripe, _direction, _params, _padding)
+end
+
+function AutoLayout:refreshSizeSuit()
+	if self.sizeSuitEnbale then
+		if self:getDirection() == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+			self:setTouchEnabled(self.box:getContentSize().height>self:getViewSize().height)
+		else
+			self:setTouchEnabled(self.box:getContentSize().width>self:getViewSize().width)
+		end
+	else
+		self:setTouchEnabled(true)
+	end	
+end
+
+function AutoLayout:layout(_movetoend, _ani)
+	-- 计算偏移,重新布局
+	local length = 0
+	local offset = 0
+	if self:getDirection() == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+		length = self.box:getContentSize().height
+		offset = self.box:getPositionY()
+		self.box:layout(self:getDirection(), {w=self:getViewSize().width})
+	else
+		length = self.box:getContentSize().width
+		offset = self.box:getPositionX()
+		self.box:layout(self:getDirection(), {h=self:getViewSize().height})
+	end
+	self:addTouchNode()
+
+	-- 调整位置
+	if _movetoend == true then
+		self:gotoEnd(_ani)
+	elseif _movetoend == false then
+		self:gotoBegin(_ani)
+	else
+		if not self.layouted then
+			self:gotoBegin(_ani)
+		else
+			if self:getDirection() == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+				self:setContentOffset(-(self.box:getContentSize().height-length)+offset, _ani)
+			else
+				self:setContentOffset(-(self.box:getContentSize().width-length)+offset, _ani)
+			end	
+		end
+	end
+
+	self:refreshSizeSuit()
+
+	self.layouted = true
+end
+
+
+return AutoLayout
---
+++ BoxLayout.lua
@@ -0,0 +1,194 @@
+--
+-- Author: wangbilt<wangbilt@gmail.com>
+-- Date: 2015-05-12 14:30:09
+--
+
+local BoxLayout = class("BoxLayout", function()
+	return cc.Node:create()
+end)
+
+function BoxLayout:ctor()
+	self:setAnchorPoint(cc.p(0.5,0.5))
+	self.item = {}
+	
+	if DEBUG_BOX_LAYOUT then
+	    self.bg = display.newColorLayer(ccc4(100,100,100,255))
+	        :addTo(self,-1)
+    end
+end
+
+function BoxLayout:count()
+	return #(self.item)
+end
+
+function BoxLayout:clear()
+	self.item = {}
+	self:removeAllChildren()
+end
+
+function BoxLayout:insert(_item, _index, _params)
+	_params = _params or {}
+	_params.l= _params.l or 0
+	_params.r= _params.r or 0
+	_params.t= _params.t or 0
+	_params.b= _params.b or 0
+
+	_item:setAnchorPoint(cc.p(0.5, 0.5))
+
+	local item = {item=_item, params = _params}
+	if _index then
+		table.insert(self.item, _index, item)
+	else
+		table.insert(self.item, item)
+	end
+	self:addChild(_item)
+
+	return _item
+end
+
+function BoxLayout:removeByTag(_tag, _all)
+	while true do
+		local isfind = false
+		for i,v in ipairs(self.item) do
+			if v.params.tag and v.params.tag == _tag then
+				self:remove(i)
+				isfind = true
+				break
+			end
+		end
+		if not isfind or not _all then
+			break
+		end
+	end
+end
+
+function BoxLayout:remove(_id)
+	if #(self.item) <= 0 then
+		return
+	end
+	local layout = self.item[_id]
+	table.remove(self.item, _id)
+	self:removeChild(layout.item)	
+end
+
+function BoxLayout:push(_item, _params)
+	return self:insert(_item, nil, _params)
+end
+
+function BoxLayout:pushGrid(_item_list, _stripe, _direction, _params, _padding)
+	_params = _params or {}
+	
+	if _direction == cc.SCROLLVIEW_DIRECTION_HORIZONTAL then
+		_params.align = _params.align or display.LEFT_CENTER
+	elseif _direction == cc.SCROLLVIEW_DIRECTION_VERTICAL and not _params.h then
+		_params.align = _params.align or display.CENTER_TOP
+	end
+	
+	local box = nil
+	local len = #(_item_list)
+	local size = nil
+	for i,v in ipairs(_item_list) do
+		if not box then
+			box = BoxLayout.new()
+		end
+		box:push(v, _padding)
+		if i%_stripe == 0 or i == len then
+			if _direction == cc.SCROLLVIEW_DIRECTION_HORIZONTAL and not _params.w then
+				_params.w = math.max(_params.w or 0, box:measure(_direction, _params.padding or 0).w)
+			elseif _direction == cc.SCROLLVIEW_DIRECTION_VERTICAL and not _params.h then
+				_params.h = math.max(_params.h or 0, box:measure(_direction, _params.padding or 0).h)
+			end
+			box:layout(_direction, _params)
+			self:push(box)
+			box = nil
+		end
+	end
+end
+
+function BoxLayout:pop()
+	return self:remove(#(self.item))
+end
+
+function BoxLayout:getItem(_id)
+	return assert(self.item[_id])
+end
+
+function BoxLayout:measure(_direction, _padding)
+	local w,h = 0,0
+	for i,v in ipairs(self.item) do
+		if _direction == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+			w = math.max(w, v.item:getContentSize().width+v.params.l+v.params.r) 
+			h = h + v.item:getContentSize().height+v.params.t+v.params.b
+		elseif _direction == cc.SCROLLVIEW_DIRECTION_HORIZONTAL then
+			h = math.max(h, v.item:getContentSize().height+v.params.t+v.params.b) 
+			w = w + v.item:getContentSize().width+v.params.l+v.params.r
+		end
+	end
+	if _direction == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+		h = h + (#(self.item) - 1) * _padding
+	elseif _direction == cc.SCROLLVIEW_DIRECTION_HORIZONTAL then
+		w = w + (#(self.item) - 1) * _padding
+	end
+
+	return {w=w,h=h}
+end
+
+function BoxLayout:layout(_direction, _params)
+	_params = _params or {}
+	_params.padding = _params.padding or 0
+	_params.align   = _params.align or display.CENTER
+	
+	local src_size = self:measure(_direction, _params.padding)
+
+	if _params.w then
+		_params.w = math.max(_params.w, src_size.w)
+	else
+		_params.w = src_size.w
+	end
+
+	if _params.h then
+		_params.h = math.max(_params.h, src_size.h)
+	else
+		_params.h = src_size.h
+	end
+
+	self:setContentSize(cc.size(_params.w, _params.h))
+	
+	if DEBUG_BOX_LAYOUT then
+	    self.bg:setContentSize(cc.size(_params.w, _params.h))
+    end
+
+    if _direction == cc.SCROLLVIEW_DIRECTION_HORIZONTAL then
+    	self:hlayout(_params, src_size.w, display.ANCHOR_POINTS[_params.align])
+	elseif _direction == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+    	self:vlayout(_params, src_size.h, display.ANCHOR_POINTS[_params.align])
+    end
+end
+
+function BoxLayout:hlayout(_params, _w, _anchor)
+	local x = _anchor.x*(_params.w - _w)
+	local y = 0
+	local size = nil
+	for i,v in ipairs(self.item) do
+		size = v.item:getContentSize()
+		y = _anchor.y * _params.h + (1 - v.item:getAnchorPoint().y - _anchor.y) * size.height
+		x = x + v.params.l + size.width/2
+		v.item:setPosition(x,y)
+		x = x + size.width/2 + v.params.r + _params.padding
+	end
+end
+
+function BoxLayout:vlayout(_params, _h, _anchor)
+	local x = 0
+	local y = _h - (_anchor.y * (_params.h - _h))
+	local size = nil
+	for i,v in ipairs(self.item) do
+		size = v.item:getContentSize()
+		x = _anchor.x * _params.w + (1 - v.item:getAnchorPoint().x - _anchor.x) * size.width
+		y = y - (v.params.t + size.height/2)
+		v.item:setPosition(x,y)
+		y = y - (size.height/2 + v.params.b + _params.padding)
+	end
+end
+
+return BoxLayout
