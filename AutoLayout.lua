2b5816166fb3e9ee5d6d57e6cae0ebbdf4a14fbf
justbilt <wangbilt@gmail.com>
2015-08-07 12:10:41 +0800

    拆离了下拉刷新的逻辑到 DropRefreshLayout 中, 代码总算可以看了

--- AutoLayout.lua
+++ AutoLayout.lua
@@ -22,7 +22,6 @@ end
 
 function AutoLayout:clear()
 	self.box:clear()
-	self.text = nil
 end
 
 function AutoLayout:count()
@@ -86,64 +85,20 @@ function AutoLayout:pushGrid(_item_list, _stripe, _direction, _params, _padding)
 	return self.box:pushGrid(_item_list, _stripe, _direction, _params, _padding)
 end
 
-function AutoLayout:refreshSizeSuit()
-	if self.sizeSuitEnbale then
-		if self:getDirection() == cc.SCROLLVIEW_DIRECTION_VERTICAL then
-			self:setTouchEnabled(self.box:getContentSize().height>self:getViewSize().height)
-		else
-			self:setTouchEnabled(self.box:getContentSize().width>self:getViewSize().width)
-		end
+function AutoLayout:isNeedScroll()
+	if self:getDirection() == cc.SCROLLVIEW_DIRECTION_VERTICAL then
+		return self.box:getContentSize().height>self:getViewSize().height
 	else
-		self:setTouchEnabled(true)
+		return self.box:getContentSize().width>self:getViewSize().width
 	end	
 end
 
-function AutoLayout:disableLoadMore()
-	self.isEnableLoadMore = false
-	self.loadMoreCallfunc = nil
-	if self.text then
-		self.text:setVisible(false)
-	end
-end
-
-function AutoLayout:enableLoadMore(_callfunc)
-	self.isEnableLoadMore = true
-	self.loadMoreCallfunc = _callfunc
-	if self.text then
-		self.text:setVisible(true)
-	end
-end
-
-function AutoLayout:onTouch_(event)
-	if self.isEnableLoadMore then
-		if "began" == event.name then
-			print("onTouch_:",self.box:getPositionY())
-			self.start_calc = self.box:getPositionY() >= 0
-			self.text:setVisible(self.start_calc)
-		end
-		if self.start_calc then
-			if "moved" == event.name then
-				local posy = self.box:getPositionY()
-				local load = posy > 200
-				if self.load ~= load then
-					if load then
-						self.text:setString("松手加载更多")
-					else
-						self.text:setString("上拉加载")
-					end
-					self.load = load
-				end
-			elseif "ended" == event.name then
-				print("AutoLayout:",self.load)
-				self.load = self.box:getPositionY() > 200
-				if self.load then
-					self.loadMoreCallfunc()
-				end
-			end
-		end
-	end
-
-	return AutoLayout.super.onTouch_(self, event)
+function AutoLayout:refreshSizeSuit()
+	if self.sizeSuitEnbale then
+		self:setTouchEnabled(self:isNeedScroll())
+	else
+		self:setTouchEnabled(true)
+	end	
 end
 
 function AutoLayout:layout(_movetoend, _ani)
@@ -175,20 +130,6 @@ function AutoLayout:layout(_movetoend, _ani)
 			else
 				self:setContentOffset(-(self.box:getContentSize().width-length)+offset, _ani)
 			end	
-		end
-	end
-
-	if not self.text then
-	    self.text = cc.ui.UILabel.new({text = "", size = 99, color = display.COLOR_WHITE})
-		    :addTo(self.box,99999)
-		    :align(display.CENTER_TOP)
-	end
-	self.text:pos(self.box:getContentSize().width/2, -100)
-	if self.isEnableLoadMore then
-		if self:getDirection() == cc.SCROLLVIEW_DIRECTION_VERTICAL then
-			self.text:setVisible(self.box:getContentSize().height>self:getViewSize().height)
-		else
-			self.text:setVisible(self.box:getContentSize().width>self:getViewSize().width)
 		end
 	end
 
---
+++ DropRefreshLayout.lua
@@ -0,0 +1,100 @@
+--
+-- Author: wangbilt@gmail.com
+-- Date: 2015-08-07 10:16:55
+--
+
+
+local DropRefreshLayout = class("DropRefreshLayout", import(".AutoLayout"))
+
+function DropRefreshLayout:ctor()
+	DropRefreshLayout.super.ctor(self)
+
+	self:setNodeEventEnabled(true)
+end
+
+function DropRefreshLayout:getBox()
+	if not self.isLoading then
+		return DropRefreshLayout.super.getBox(self)
+	end
+	return self.scrollNode:getCascadeBoundingBox()
+end
+
+local tmpMoveStartEnd = nil
+local tmpLoad = nil
+function DropRefreshLayout:onTouch_(event)
+	if not self.isLoading and self.isEnableLoadMore then
+		if "began" == event.name then
+			tmpMoveStartEnd = self.box:getPositionY() >= 0
+			print(self.box:getPositionY())
+			self.dropNode:setVisible(self:isNeedScroll() and tmpMoveStartEnd)
+		end
+		if tmpMoveStartEnd then
+			if "moved" == event.name then
+				local load = self.box:getPositionY() > self.dropNode:getContentSize().height
+				if tmpLoad ~= load then
+					if load then
+						self.loadMoreCallfunc("over")
+					else
+						self.loadMoreCallfunc("begin")
+					end
+					tmpLoad = load
+				end
+			elseif "ended" == event.name then
+				if self.box:getPositionY() > self.dropNode:getContentSize().height then
+					self.loadMoreCallfunc("end")
+					self.isLoading = true
+				end
+			end
+		end
+	end
+
+	return DropRefreshLayout.super.onTouch_(self, event)
+end
+
+
+function DropRefreshLayout:disable()
+	self.isEnableLoadMore = false
+	self.loadMoreCallfunc = nil
+	self.dropNode:removeFromParent()
+	self.dropNode:release()
+	self.dropNode = nil
+	self.isLoading = false
+	self:gotoEnd()
+end
+
+function DropRefreshLayout:enable(_node, _callfunc)
+	self.dropNode = _node
+	self.dropNode:retain()
+	self.dropNode:setVisible(false)
+
+	self.isEnableLoadMore = true
+	self.loadMoreCallfunc = _callfunc
+end
+
+function DropRefreshLayout:layout(_movetoend, _ani)
+	self.isLoading = false
+	if self.dropNode then
+		self.loadMoreCallfunc("reset")
+	end
+	
+	DropRefreshLayout.super.layout(self, _movetoend, _ani)
+
+	if self.dropNode then
+		if not self.dropNode:getParent() then
+		    self.dropNode:addTo(self.box,99999)
+			    :align(display.CENTER_TOP)
+		end
+		self.dropNode:setPositionX(self.box:getContentSize().width/2)
+	end	
+end
+
+
+function DropRefreshLayout:onExit()
+	if self.dropNode then	
+		self.dropNode:release()
+		self.dropNode = nil
+	end
+end
+
+
+return DropRefreshLayout
